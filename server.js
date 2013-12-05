5ce27a080769a4168cf335086cf5f4d5d5975ee5
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2013-12-05 23:48:05 +0100

    Large update handling redirects, state, and more..

--- server.js
+++ server.js
@@ -45,16 +45,9 @@ var o = new FeideConnect({
 
 });
 
-
-// var scopes = {
-// 	request: ["https://www.googleapis.com/auth/userinfo.email"],
-// 	require: ["https://www.googleapis.com/auth/userinfo.email"]
-// };
-
-
 app.use('/callback', o.getAuthenticationMiddleware().callback() );
 
-app.use('/', o.getAuthenticationMiddleware() );
+// app.use('/', o.getAuthenticationMiddleware() );
 app.use('/test', o.getAuthenticationMiddleware()
 	.requireScopes(['userinfo'])
 	.authenticate()
@@ -92,63 +85,6 @@ app.get('/test', function(req, res){
 	res.setHeader('Content-Length', body.length);
 	res.end(body);
 });
-
-
- // o.ajax({
- //     url: "https://www.googleapis.com/oauth2/v1/userinfo",
- //     oauth: {
- //         scopes: {
- //             request: ["https://www.googleapis.com/auth/userinfo.email"],
- //             require: ["https://www.googleapis.com/auth/userinfo.email"]
- //         }
- //     },
- //     dataType: 'json',
- //     success: function(data) {
- //         console.log("Response (google):");
- //         console.log(data);
- //         $(".loader-hideOnLoad").hide();
- //     }
- // });
-
-// // console.log(po2);
-
-// passport.use('provider', new po2.Strategy({
-//     "authorizationURL": 'https://core.uwap.org/api/oauth/authorization',
-//     "tokenURL": 'https://core.uwap.org/api/oauth/token',
-//     "clientID": 'c74e2395-3712-4c53-b488-e0108af48952',
-//     "clientSecret": 'bde08ff3-eee2-4369-9e6b-5e17e2579793',
-//     "callbackURL": 'http://0.0.0.0:3000/callback'
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-
-
-//     console.log("Got the token", accessToken, refreshToken, profile);
-//     done();
-
-//     // User.findOrCreate(..., function(err, user) {
-//     //   done(err, user);
-//     // });
-//   }
-// ));
-
-// Redirect the user to the OAuth 2.0 provider for authentication.  When
-// complete, the provider will redirect the user back to the application at
-//     /auth/provider/callback
-// app.get('/auth/provider', passport.authenticate('provider'));
-
-// // The OAuth 2.0 provider has redirected the user back to the application.
-// // Finish the authentication process by attempting to obtain an access
-// // token.  If authorization was granted, the user will be logged in.
-// // Otherwise, authentication has failed.
-// app.get('/callback', 
-// passport.authenticate('provider', { successRedirect: '/',
-//                                       failureRedirect: '/login' }));
-
-
-// app.get('/auth/provider',
-//   passport.authenticate('provider', { scope: ['email', 'sms'] })
-// );
-
 
 
 
